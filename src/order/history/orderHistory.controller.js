88da8e4035360952939247630f85cc609f7fabf7
Reshma Sumbe <reshma.sumbe@finche.in>
2022-04-24 17:43:03 +0530

    order history api

---
+++ src/order/history/orderHistory.controller.js
@@ -0,0 +1,35 @@
+import OrderHistoryService from './orderHistory.service.js';
+
+const orderHistoryService = new OrderHistoryService();
+
+class OrderHistoryController {
+    
+    /**
+    * get order list
+    * @param {*} req    HTTP request object
+    * @param {*} res    HTTP response object
+    * @param {*} next   Callback argument to the middleware function
+    * @return {callback}
+    */
+    getOrdersList(req, res, next) {
+        const { query, user } = req;
+
+        orderHistoryService.getOrdersList(query, user).then(response => {
+            if(response?.length)
+                res.json([ ...response ]);
+            else
+                res.status(404).json([
+                    {
+                        error: { 
+                            message: "No data found", 
+                            status: "BAP_010", 
+                        }
+                    }
+                ]);
+        }).catch((err) => {
+            next(err);
+        });
+    }
+}
+
+export default OrderHistoryController;
--- src/order/order.routes.js
+++ src/order/order.routes.js
@@ -2,28 +2,33 @@ import {Router} from 'express';
 import { authentication } from '../middlewares/index.js';
 
 import ConfirmOrderController from './confirm/confirmOrder.controller.js';
+import OrderHistoryController from './history/orderHistory.controller.js';
 
 const rootRouter = new Router();
 
 const confirmOrderController = new ConfirmOrderController();
+const orderHistoryController = new OrderHistoryController();
 
 //#region confirm order
-// confirm order
+
+// confirm order v1
 rootRouter.post(
     '/v1/confirm_order', 
     authentication(),
     confirmOrderController.confirmOrder,
 );
 
-// confirm order
+// confirm order v2
 rootRouter.post(
     '/v2/confirm_order', 
     authentication(),
     confirmOrderController.confirmMultipleOrder,
 );
 
-// on confirm order
+// on confirm order v1
 rootRouter.get('/v1/on_confirm_order', authentication(), confirmOrderController.onConfirmOrder);
+
+// on confirm order v2
 rootRouter.get('/v2/on_confirm_order', authentication(), confirmOrderController.onConfirmMultipleOrder);
 
 //#endregion
@@ -32,6 +37,7 @@ rootRouter.get('/v2/on_confirm_order', authentication(), confirmOrderController.
 //#endregion
 
 //#region order history
+rootRouter.get('/v1/orders', authentication(), orderHistoryController.getOrdersList);
 //#endregion
 
 export default rootRouter;
