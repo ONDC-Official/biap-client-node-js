ead05143bf38b4f38cdedacd069f58c9cf7f91a0
Ajinkya Salve <ajinkya@finche.in>
2022-04-29 19:48:12 +0530

    cancel and on_cancel apis

---
+++ src/order/cancel/bppCancel.service.js
@@ -0,0 +1,36 @@
+import { bppCancel } from "../../utils/bppApis/index.js";
+import { getBaseUri } from "../../utils/urlHelper.js";
+
+class BppConfirmService {
+    
+    /**
+     * 
+     * @param {String} bppUri 
+     * @param {Object} context 
+     * @param {String} orderId 
+     * @param {String} cancellationReasonId 
+     * @returns 
+     */
+    async cancelOrder(bppUri, context, orderId, cancellationReasonId) {
+        try {
+
+            const cancelRequest = {
+                context: context,
+                message: {
+                    order_id: orderId,
+                    cancellation_reason_id: cancellationReasonId
+                }
+            }
+            bppUri = getBaseUri(bppUri);
+            
+            const response = await bppCancel(bppUri, cancelRequest);
+            
+            return [{ context: context, message: response.message }];
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+}
+
+export default BppConfirmService;
