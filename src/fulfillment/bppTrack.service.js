64988fa7267acf41206f39d672d75f97fa9b8f1b
Reshma Sumbe <reshma.sumbe@finche.in>
2022-05-05 12:42:15 +0530

    missing file
    (cherry picked from commit 83f32e938d8b0f03c77beb2ee5d9f3728d5ddb21)

---
+++ src/fulfillment/bppTrack.service.js
@@ -0,0 +1,34 @@
+import { bppTrack } from "../utils/bppApis/index.js";
+import { getBaseUri } from "../utils/urlHelper.js";
+
+class BppTrackService {
+    
+    /**
+     * track order
+     * @param {String} bppUri 
+     * @param {Object} context 
+     * @param {Object} trackRequest 
+     * @returns 
+     */
+    async track(bppUri, context = {}, request = {}) {
+        try {
+
+            const trackRequest = {
+                context: context,
+                message: {
+                    ...request.message
+                }
+            }
+            bppUri = getBaseUri(bppUri);
+            
+            const response = await bppTrack(bppUri, trackRequest);
+            
+            return { context: context, message: response.message };
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+}
+
+export default BppTrackService;
