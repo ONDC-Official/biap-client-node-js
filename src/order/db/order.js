4e2781538a88497f4d8ff15f84e34a8d4bd0828f
Reshma Sumbe <reshma.sumbe@finche.in>
2022-08-05 16:46:24 +0530

    update order schema structure - remove default values

--- src/order/db/order.js
+++ src/order/db/order.js
@@ -9,43 +9,43 @@ const AddOnsSchema = new mongoose.Schema(
 
 const OrganizationSchema = new mongoose.Schema(
     {
-        name: { type: String, default: null },
-        cred: { type: String, default: null },
+        name: { type: String },
+        cred: { type: String },
     },
     { _id: false }
 );
 
 const AddressSchema = new mongoose.Schema(
     {
-        door: { type: String, default: null },
-        name: { type: String, default: null },
-        building: { type: String, default: null },
-        street: { type: String, default: null },
-        locality: { type: String, default: null },
-        ward: { type: String, default: null },
-        city: { type: String, default: null },
-        state: { type: String, default: null },
-        country: { type: String, default: null },
-        areaCode: { type: String, default: null }
+        door: { type: String },
+        name: { type: String },
+        building: { type: String },
+        street: { type: String },
+        locality: { type: String },
+        ward: { type: String },
+        city: { type: String },
+        state: { type: String },
+        country: { type: String },
+        areaCode: { type: String }
     },
     { _id: false }
 );
 
 const TimeRangeSchema = new mongoose.Schema(
     {
-        start: { type: Date, default: null },
-        end: { type: Date, default: null },
+        start: { type: Date },
+        end: { type: Date },
     },
     { _id: false }
 );
 
 const TimeSchema = new mongoose.Schema(
     {
-        label: { type: String, default: null },
-        timestamp: { type: Date, default: null },
-        duration: { type: String, default: null },
-        range: { type: TimeRangeSchema, default: null },
-        days: { type: String, default: null },
+        label: { type: String },
+        timestamp: { type: Date },
+        duration: { type: String },
+        range: { type: TimeRangeSchema },
+        days: { type: String },
     },
     { _id: false }
 );
@@ -55,12 +55,12 @@ const BillingSchema = new mongoose.Schema(
         id: String,
         name: { type: String, required: true },
         phone: { type: String, required: true },
-        organization: { type: OrganizationSchema, default: null },
-        address: { type: AddressSchema, default: null },
-        email: { type: String, default: null },
-        time: { type: TimeSchema, default: null },
-        taxNumber: { type: String, default: null },
-        locationId: { type: String, default: null }
+        organization: { type: OrganizationSchema },
+        address: { type: AddressSchema },
+        email: { type: String },
+        time: { type: TimeSchema },
+        taxNumber: { type: String },
+        locationId: { type: String }
     },
     { _id: false, timestamps: true }
 );
@@ -68,75 +68,75 @@ const BillingSchema = new mongoose.Schema(
 const DescriptorSchema = new mongoose.Schema(
     {
         name: String,
-        phone: { type: String, default: null },
-        email: { type: String, default: null },
-        code: { type: String, default: null },
-        symbol: { type: String, default: null },
-        shortDesc: { type: String, default: null },
-        longDesc: { type: String, default: null },
-        images: { type: [String], default: null },
-        audio: { type: String, default: null },
-        "3d_render": { type: String, default: null }
+        phone: { type: String },
+        email: { type: String },
+        code: { type: String },
+        symbol: { type: String },
+        shortDesc: { type: String },
+        longDesc: { type: String },
+        images: { type: [String] },
+        audio: { type: String },
+        "3d_render": { type: String }
     },
     { _id: false }
 );
 
 const StateSchema = new mongoose.Schema(
     {
-        descriptor: { type: DescriptorSchema, default: null },
-        updatedAt: { type: Date, default: null },
-        updatedBy: { type: String, default: null },
+        descriptor: { type: DescriptorSchema },
+        updatedAt: { type: Date },
+        updatedBy: { type: String },
     },
     { _id: false }
 );
 
 const PersonSchema = new mongoose.Schema(
     {
-        name: { type: String, default: null },
-        image: { type: String, default: null },
-        dob: { type: Date, default: null },
-        gender: { type: String, default: null },
-        cred: { type: String, default: null },
-        tags: { type: Map, default: null },
+        name: { type: String },
+        image: { type: String },
+        dob: { type: Date },
+        gender: { type: String },
+        cred: { type: String },
+        tags: { type: Map },
     },
     { _id: false }
 );
 
 const VehicleSchema = new mongoose.Schema(
     {
-        category: { type: String, default: null },
-        capacity: { type: Number, default: null },
-        make: { type: String, default: null },
-        model: { type: String, default: null },
-        size: { type: String, default: null },
-        variant: { type: String, default: null },
-        color: { type: String, default: null },
-        energyType: { type: String, default: null },
-        registration: { type: String, default: null }
+        category: { type: String },
+        capacity: { type: Number },
+        make: { type: String },
+        model: { type: String },
+        size: { type: String },
+        variant: { type: String },
+        color: { type: String },
+        energyType: { type: String },
+        registration: { type: String }
     },
     { _id: false }
 );
 
 const CitySchema = new mongoose.Schema(
     {
-        name: { type: String, default: null },
-        code: { type: String, default: null }
+        name: { type: String },
+        code: { type: String }
     },
     { _id: false }
 );
 
 const CountrySchema = new mongoose.Schema(
     {
-        name: { type: String, default: null },
-        code: { type: String, default: null }
+        name: { type: String },
+        code: { type: String }
     },
     { _id: false }
 );
 
 const ScalarRangeSchema = new mongoose.Schema(
     {
-        min: { type: mongoose.Decimal128, default: null },
-        max: { type: mongoose.Decimal128, default: null },
+        min: { type: mongoose.Decimal128 },
+        max: { type: mongoose.Decimal128 },
     },
     { _id: false }
 );
@@ -145,87 +145,87 @@ const ScalarSchema = new mongoose.Schema(
     {
         value: { type: mongoose.Decimal128, required: true },
         unit: { type: String, required: true },
-        type: { type: String, enum: ['CONSTANT', 'VARIABLE'], default: null },
-        estimatedValue: { type: mongoose.Decimal128, default: null },
-        computedValue: { type: mongoose.Decimal128, default: null },
-        range: { type: ScalarRangeSchema, default: null },
+        type: { type: String, enum: ['CONSTANT', 'VARIABLE'] },
+        estimatedValue: { type: mongoose.Decimal128 },
+        computedValue: { type: mongoose.Decimal128 },
+        range: { type: ScalarRangeSchema },
     },
     { _id: false }
 );
 
 const CircleSchema = new mongoose.Schema(
     {
-        radius: { type: ScalarSchema, default: null },
+        radius: { type: ScalarSchema },
     },
     { _id: false }
 );
 
 const LocationSchema = new mongoose.Schema(
     {
-        id: { type: String, default: null },
-        descriptor: { type: DescriptorSchema, default: null },
-        gps: { type: String, default: null },
-        address: { type: AddressSchema, default: null },
-        stationCode: { type: String, default: null },
-        city: { type: CitySchema, default: null },
-        country: { type: CountrySchema, default: null },
-        circle: { type: CircleSchema, default: null },
-        polygon: { type: String, default: null },
-        "3dspace": { type: String, default: null }
+        id: { type: String },
+        descriptor: { type: DescriptorSchema },
+        gps: { type: String },
+        address: { type: AddressSchema },
+        stationCode: { type: String },
+        city: { type: CitySchema },
+        country: { type: CountrySchema },
+        circle: { type: CircleSchema },
+        polygon: { type: String },
+        "3dspace": { type: String }
     },
     { _id: false }
 );
 
 const ContactSchema = new mongoose.Schema(
     {
-        phone: { type: String, default: null },
-        email: { type: String, default: null },
-        tags: { type: Map, default: null }
+        phone: { type: String },
+        email: { type: String },
+        tags: { type: Map }
     },
     { _id: false }
 );
 
 const FulfillmentStartSchema = new mongoose.Schema(
     {
-        location: { type: LocationSchema, default: null },
-        time: { type: TimeSchema, default: null },
-        instructions: { type: DescriptorSchema, default: null },
-        contact: { type: ContactSchema, default: null }
+        location: { type: LocationSchema },
+        time: { type: TimeSchema },
+        instructions: { type: DescriptorSchema },
+        contact: { type: ContactSchema }
     },
     { _id: false }
 );
 
 const FulfillmentEndSchema = new mongoose.Schema(
     {
-        location: { type: LocationSchema, default: null },
-        time: { type: TimeSchema, default: null },
-        instructions: { type: DescriptorSchema, default: null },
-        contact: { type: ContactSchema, default: null }
+        location: { type: LocationSchema },
+        time: { type: TimeSchema },
+        instructions: { type: DescriptorSchema },
+        contact: { type: ContactSchema }
     },
     { _id: false }
 );
 
 const CustomerSchema = new mongoose.Schema(
     {
-        person: { type: PersonSchema, default: null },
-        contact: { type: ContactSchema, default: null }
+        person: { type: PersonSchema },
+        contact: { type: ContactSchema }
     },
     { _id: false }
 );
 
 const FulfillmentSchema = new mongoose.Schema(
     {
-        id: { type: String, default: null },
-        type: { type: String, default: null },
-        state: { type: StateSchema, default: null },
-        tracking: { type: Boolean, default: null },
-        agent: { type: PersonSchema, default: null },
-        vehicle: { type: VehicleSchema, default: null },
-        start: { type: FulfillmentStartSchema, default: null },
-        end: { type: FulfillmentEndSchema, default: null },
-        purpose: { type: String, default: null },
-        customer: { type: CustomerSchema, default: null },
-        tags: { type: Map, default: null },
+        id: { type: String },
+        type: { type: String },
+        state: { type: StateSchema },
+        tracking: { type: Boolean },
+        agent: { type: PersonSchema },
+        vehicle: { type: VehicleSchema },
+        start: { type: FulfillmentStartSchema },
+        end: { type: FulfillmentEndSchema },
+        purpose: { type: String },
+        customer: { type: CustomerSchema },
+        tags: { type: Map },
     },
     { _id: false }
 );
@@ -247,8 +247,8 @@ const ProviderSchema = new mongoose.Schema(
 
 const ItemQuantityAllocatedSchema = new mongoose.Schema(
     {
-        count: { type: Number, default: null },
-        measure: { type: ScalarSchema, default: null },
+        count: { type: Number },
+        measure: { type: ScalarSchema },
     },
     { _id: false }
 );
@@ -270,67 +270,68 @@ const OfferSchema = new mongoose.Schema(
 
 const PriceSchema = new mongoose.Schema(
     {
-        currency: { type: String, default: null },
-        value: { type: String, default: null },
-        estimated_value: { type: String, default: null },
-        computed_value: { type: String, default: null },
-        listed_value: { type: String, default: null },
-        offered_value: { type: String, default: null },
-        minimum_value: { type: String, default: null },
-        maximum_value: { type: String, default: null },
+        currency: { type: String },
+        value: { type: String },
+        estimated_value: { type: String },
+        computed_value: { type: String },
+        listed_value: { type: String },
+        offered_value: { type: String },
+        minimum_value: { type: String },
+        maximum_value: { type: String },
     },
     { _id: false }
 );
 
 const QuotationBreakupSchema = new mongoose.Schema(
     {
-        type: { type: String, enum: ['item', 'offer', 'add-on', 'fulfillment'], default: null },
-        refId: { type: String, default: null },
-        title: { type: String, default: null },
-        price: { type: PriceSchema, default: null }
+        type: { type: String, enum: ['item', 'offer', 'add-on', 'fulfillment'] },
+        refId: { type: String },
+        title: { type: String },
+        price: { type: PriceSchema }
     },
     { _id: false }
 );
 
 const QuotationSchema = new mongoose.Schema(
     {
-        price: { type: PriceSchema, default: null },
-        breakup: { type: [QuotationBreakupSchema], default: null },
-        ttl: { type: String, default: null }
+        price: { type: PriceSchema },
+        breakup: { type: [QuotationBreakupSchema] },
+        ttl: { type: String }
     },
     { _id: false }
 );
 
 const PaymentSchema = mongoose.Schema(
     {
-        uri: { type: String, default: null },
-        tlMethod: { type: String, enum: ['http/get', 'http/post'], default: null },
-        params: { type: Map, default: null },
-        type: { type: String, enum: ['ON-ORDER', 'PRE-FULFILLMENT', 'ON-FULFILLMENT', 'POST-FULFILLMENT'], default: null },
-        status: { type: String, enum: ['PAID', 'NOT-PAID'], default: null },
-        time: { type: TimeSchema, default: null }
+        uri: { type: String },
+        tlMethod: { type: String, enum: ['http/get', 'http/post'] },
+        params: { type: Map },
+        type: { type: String, enum: ['ON-ORDER', 'PRE-FULFILLMENT', 'ON-FULFILLMENT', 'POST-FULFILLMENT'] },
+        status: { type: String, enum: ['PAID', 'NOT-PAID'] },
+        time: { type: TimeSchema }
     },
     { _id: false }
 );
 
 const OrderSchema = new mongoose.Schema(
     {
-        provider: { type: ProviderSchema, default: null },
-        items: { type: [ItemsSchema], default: null },
-        addOns: { type: [AddOnsSchema], default: null },
-        offers: { type: [OfferSchema], default: null },
-        billing: { type: BillingSchema, default: null },
-        fulfillments: { type: [FulfillmentSchema], default: null },
-        quote: { type: QuotationSchema, default: null },
-        payment: { type: PaymentSchema, default: null },
-        id: { type: String, default: null },
-        state: { type: String, default: null }, //["PENDING-CONFIRMATION", "Ordered", "CANCELLED", "Pending", "Active", "Processing"]
+        provider: { type: ProviderSchema },
+        items: { type: [ItemsSchema] },
+        addOns: { type: [AddOnsSchema] },
+        offers: { type: [OfferSchema] },
+        billing: { type: BillingSchema },
+        fulfillments: { type: [FulfillmentSchema] },
+        quote: { type: QuotationSchema },
+        payment: { type: PaymentSchema },
+        id: { type: String },
+        state: { type: String }, //["PENDING-CONFIRMATION", "Ordered", "CANCELLED", "Pending", "Active", "Processing"]
         userId: String,
-        transactionId: { type: String, default: null },
-        messageId: { type: String, default: null },
-        parentOrderId: { type: String, default: null },
+        transactionId: { type: String },
+        messageId: { type: String },
+        parentOrderId: { type: String },
         paymentStatus: { type: String, enum: ['PAID', 'NOT-PAID'], default: null },
-        bppId: { type: String, default: null },
+        bppId: { type: String },
+        bapOrderId: { type: String }
     },
     { _id: true, timestamps: true }
 );
