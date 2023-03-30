bfe36a54cbba4efa4f0b5fa9e18e153c7c21d4f5
akshay <akshay@wemotiveforge.com>
2023-03-30 12:44:35 +0530

    fix: delete ward

--- src/order/init/bppInit.service.js
+++ src/order/init/bppInit.service.js
@@ -30,10 +30,11 @@ class BppInitService {
 
             delete order.billing_info.address.tag
             delete order.billing_info.address.street
-            delete order.billing_info.address.door
+            delete order.billing_info.address.ward
             delete order.delivery_info.location.address.tag
             delete order.delivery_info.location.address.street
             delete order.delivery_info.location.address.door
+            delete order.delivery_info.location.address.ward
 
             const initRequest = {
                 context: context,
