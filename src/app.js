d33392c2b32824c02e2a0cbf27a3bcd53981844f
akshay <akshay@wemotiveforge.com>
2023-09-18 14:57:05 +0530

    chg: payload size handling

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 loadEnvVariables();
 initializeFirebase();
 
-app.use(express.json());
+//app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
