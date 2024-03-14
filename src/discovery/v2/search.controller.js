400af10710855fc166b0e0bb9a40b76cc402db3c
Ritu <ritu@WILASSETA0171.local>
2024-03-14 17:43:02 +0530

    attribute structure fix

--- src/discovery/v2/search.controller.js
+++ src/discovery/v2/search.controller.js
@@ -111,8 +111,7 @@ class SearchController {
         if (!response || response === null) {
           throw new NoRecordFoundError("No result found");
         } else {
-          req.body.responseData = response;
-          next();
+          return res.send(response);
         }
       })
       .catch((err) => {
