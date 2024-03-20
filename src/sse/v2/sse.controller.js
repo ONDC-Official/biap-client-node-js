c79eae2e8b84406bff1b507e39e1259594fc0ca6
Rupinder <rupinder.singh@thewitslab.com>
2024-03-20 16:48:21 +0530

    fix: onevent change

--- src/sse/v2/sse.controller.js
+++ src/sse/v2/sse.controller.js
@@ -15,8 +15,7 @@ class SseController {
     * @param {*} next   Callback argument to the middleware function
     * @return {callback}
     */
-    async 
-    onEvent(req, res, next) {
+    async onEvent(req, res, next) {
 
         try {
             const { query = {} } = req;
